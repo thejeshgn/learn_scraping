db5d7b7c76c310233ece80cc9c3674891079cb0b
Thejesh GN <i@thejeshgn.com>
2021-02-01 15:28:19 +0530

    Create exercise.py

---
+++ exercise_01/exercise.py
@@ -0,0 +1,13 @@
+# Question: Get the title of the given url
+
+def get_title(url):
+    # This function should return the title of the url
+    return None
+
+def main():
+    """Runs the program."""
+    url = "https://www.google.com/"    
+    print(get_title(url))
+
+if __name__ == '__main__':
+    main()
